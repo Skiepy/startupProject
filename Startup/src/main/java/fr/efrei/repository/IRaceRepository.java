1fa8dc2d2a29dc0e46b2a574a6882b2477702cbd
Yanis M <yanis.millier@efrei.net>
2022-11-30 15:36:57 +0100

    Delete Startup directory

--- Startup/src/main/java/fr/efrei/repository/IRaceRepository.java
+++
@@ -1,9 +0,0 @@
-package fr.efrei.repository;
-import fr.efrei.domain.Race;
-import java.util.List;
-
-public interface IRaceRepository extends IRepository<Race, String> {
-    // create, read, update, delete
-
-    public List<Race> getAll();
-}
--- Startup/src/main/java/fr/efrei/repository/IRepository.java
+++
@@ -1,8 +0,0 @@
-package fr.efrei.repository;
-
-public interface IRepository<T, ID> {
-    public T create(T t);
-    public T read(ID id);
-    public T update(T t);
-    public boolean delete(ID i);
-}
