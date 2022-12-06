53cfa04cc1e659627d576c0ebbc4abe8b2a7c122
Zbram <75671598+Zbram@users.noreply.github.com>
2022-12-06 12:58:18 +0200

    update menu

--- src/main/java/fr/efrei/views/Main.java
+++ src/main/java/fr/efrei/views/Main.java
@@ -7,18 +7,15 @@ import fr.efrei.repository.RaceRepository;
 public class Main {
     public static RaceRepository repository = RaceRepository.getRepository();
 
-    public static void loadObjects(){
-        Race race1 = RaceFactory.createRace("Indian");
-        Race race2 = RaceFactory.createRace("White");
-        Race race3 = RaceFactory.createRace("SpaceMan");
-        repository.create(race1);
-        repository.create(race2);
-        repository.create(race3);
-    }
+//   public static void loadObjects(){
+//         Race race1 = RaceFactory.createRace("Indian");
+//         Race race3 = RaceFactory.createRace("SpaceMan");
+//         repository.create(race1);
+//         repository.create(race2);
+//         repository.create(race3);
+//     }  
 
     public static void main(String[] args) {
-
-
         new Menu().loadMenu();
     }
 }
