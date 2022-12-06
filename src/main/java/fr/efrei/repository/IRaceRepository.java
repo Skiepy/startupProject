72c44bde8d5d52ce9b1396467c9f79c97d34df83
Skiepy <72888827+Skiepy@users.noreply.github.com>
2022-12-06 18:38:02 +0100

    Update IRaceRepository.java

--- src/main/java/fr/efrei/repository/IRaceRepository.java
+++ src/main/java/fr/efrei/repository/IRaceRepository.java
@@ -2,8 +2,6 @@ package fr.efrei.repository;
 import fr.efrei.domain.Race;
 import java.util.List;
 
-public interface IRaceRepository extends IRepository<Race, String> {
-    // create, read, update, delete
-
+public interface  IRaceRepository extends IRepository<Race, String> {
     public List<Race> getAll();
 }
